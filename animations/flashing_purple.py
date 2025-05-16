0a2fab2dcf667dfc7db4622489e06aef6804ec28
Ofer Mustigman <ofer@cycognito.com>
2025-05-16 16:59:00 +0300

    changes made

--- animations/flashing_purple.py
+++ animations/flashing_purple.py
@@ -1,6 +1,8 @@
 import asyncio
+import time
 from animations.utils import set_face_color
-from main import SharedState
+from utils import SharedState
+
 
 async def animate(
         np: neopixel.NeoPixel,
@@ -8,18 +10,39 @@ async def animate(
         num_faces: int,
         layers: tuple[tuple[int, ...], ...],
         stop_event: asyncio.Event,
-        state: SharedState) -> None:
+        state: SharedState
+    ) -> None:
     
+    num_steps = 100
+    layer_ratio = num_steps / len(layers)
+    reverse_start_step = num_steps - 1
+    minimum_intensity = 50
+    frame_time_ms = 1000/30
     while True:
-        distance = await state.get()
-        if distance == {}:
-            distance = 0
-        else:
-            distance = distance['distance']
-        clamped_distance = max(0, min(distance, 1000))
-        scaled_distance = int(clamped_distance * 255 / 1000)
-        np.fill((0, 255 - scaled_distance, scaled_distance))
-        np.write()
-        await asyncio.sleep(0.01)
-    
-   
+        for i in range(num_steps):
+            if stop_event.is_set():
+                return
+            frame_start = time.time_ns()
+            for j in range(len(layers)):
+                layer_location = j * layer_ratio
+                distance = int(abs(i - layer_location))
+                intensity = max(minimum_intensity, 255 - distance*30)
+                layer_color = (intensity, 0, intensity)
+                for face in layers[j]:
+                    set_face_color(np, leds_per_face, face, layer_color)
+            np.write()
+            await asyncio.sleep_ms(int(frame_time_ms - (time.time_ns() - frame_start)/1000000))
+        for i in range(reverse_start_step, -1, -1):
+            if stop_event.is_set():
+                return
+            frame_start = time.time_ns()
+            for j in range(len(layers)):
+                layer_location = j * layer_ratio
+                distance = int(abs(i - layer_location))
+                intensity = max(minimum_intensity, 255 - distance*30)
+                layer_color = (intensity, 0, intensity)
+                for face in layers[j]:
+                    set_face_color(np, leds_per_face, face, layer_color)
+            np.write()
+            await asyncio.sleep_ms(int(frame_time_ms - (time.time_ns() - frame_start)/1000000))
+
---
+++ animations/rainbow.py
@@ -0,0 +1,35 @@
+import asyncio
+import time
+from animations.utils import set_face_color
+from utils import SharedState
+
+RAINBOW_COLORS = [
+    (255, 0, 0),
+    (255, 165, 0),
+    (255, 255, 0),
+    (0, 255, 0),
+    (0, 0, 255),
+    (75, 0, 130),
+    (148, 0, 211),
+]
+
+
+async def animate(
+        np: neopixel.NeoPixel,
+        leds_per_face: int,
+        num_faces: int,
+        layers: tuple[tuple[int, ...], ...],
+        stop_event: asyncio.Event,
+        state: SharedState
+) -> None:
+    current_color_index = 0
+    while True:
+        for i in range(len(layers)):
+            for face in layers[i]:
+                set_face_color(np, leds_per_face, face, RAINBOW_COLORS[(current_color_index+i)%len(RAINBOW_COLORS)])
+        np.write()
+        current_color_index = (current_color_index + 1) % len(RAINBOW_COLORS)
+        await asyncio.sleep_ms(1000)
+        if stop_event.is_set():
+            return
+
